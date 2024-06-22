b85fa6740d4990852ad02ffbb7669611a5d4d020
juanimartin2 <juanimartinnn3@gmail.com>
2024-06-22 02:52:23 -0300

    home centrada y retocada

--- src/app/app-home/pages/bienvenida/bienvenida.component.ts
+++ src/app/app-home/pages/bienvenida/bienvenida.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { AppHomeRoutingModule } from '../../app-home-routing.module';
 import { RouterModule } from '@angular/router';
+import { OfficeSvgComponent } from '../../../shared/office-svg/office-svg.component';
 
 @Component({
   selector: 'app-bienvenida',
   standalone: true,
   imports: [
     RouterModule,
-    AppHomeRoutingModule
+    AppHomeRoutingModule,
+    OfficeSvgComponent
   ],
   templateUrl: './bienvenida.component.html',
   styleUrl: './bienvenida.component.scss'
---
+++ src/app/shared/office-svg/office-svg.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-office-svg',
+  standalone: true,
+  imports: [],
+  templateUrl: './office-svg.component.html',
+  styleUrl: './office-svg.component.scss'
+})
+export class OfficeSvgComponent {
+
+}
